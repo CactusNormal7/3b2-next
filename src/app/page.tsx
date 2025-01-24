2d495ff0e7b582b93c801f2a523fd2e84e8b5b12
CactusNormal7 <jules.besson7@gmail.com>
2025-01-24 13:47:40 +0100

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import styles from "./page.module.css";
 
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   console.log("tasksa", tasks);
-
+  const { signIn } = useAuthActions();
   return (
     <>
-      <div>{tasks?.map((task) => <div>{task.text}</div>)}</div>
+      <button onClick={() => void signIn("github")}>Sign in with GitHub</button>
+      <div>{tasks?.map((task) => <div key={task._id}>{task.text}</div>)}</div>
     </>
   );
 }

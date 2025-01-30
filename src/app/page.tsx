c878c6c003040d1d7eee58b5dac11222b7ce297f
CactusNormal7 <jules.besson7@gmail.com>
2025-01-30 15:36:45 +0100

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import styles from "./page.module.css";
-
 import { useConvexAuth, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
@@ -11,16 +8,14 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   const { isLoading, isAuthenticated } = useConvexAuth();
-  const { user } = useUser();
+  const { user, isLoaded } = useUser(); 
 
-  console.log("isauth", isAuthenticated);
-  console.log("user", user?.id);
-  console.log("user", user);
+  if (isLoading) return <p>Loading...</p>; // Évite un rendu serveur différent du client
+  if (!isLoaded) return <p>Loading...</p>; 
 
   return (
     <>
-      <Navbar></Navbar>
-      
+      <Navbar/>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import Link from 'next/link'
 import "../styles/Navbar.css";
-import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, UserButton, UserProfile, useUser } from "@clerk/nextjs";
+import { useConvexAuth } from "convex/react";
 
 const Navbar = () => {
+  const { user, isLoaded } = useUser();
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
   return (
     <>
-      <div id="navbar-mai-div">
+      <div id="navbar-main-div">
         <section id="main-content">logo</section>
         <section id="stucked-right-part">
-          <SignInButton  mode="modal">
-            <button className="sign-in-button">Sign in</button>
-          </SignInButton>
-          <SignOutButton></SignOutButton>
+
+          <div className="nav-link-div">
+            <Link className="navbar-link" href="/">Home</Link>
+            <Link className="navbar-link" href="/">Home</Link>
+            <Link className="navbar-link" href="/">Home</Link>
+            <Link className="navbar-link" href="/">Home</Link>
+            <Link className="navbar-link" href="/">Home</Link>
+            <Link className="navbar-link" href="/">Home</Link>
+          </div>
+
+          <section className="nav-button-div">
+            {(isLoaded && !isLoading) ? (
+              !isAuthenticated ? (<>
+                <SignInButton>
+                  <button className="basic-button">Sign in</button>
+                </SignInButton>
+              </>) : (
+                <>
+                  <UserButton ></UserButton >
+                </>
+              )
+            ) : (
+              <>
+                <span>ratio</span>
+              </>
+            )}
+            <button className="basic-button">Search</button>
+          </section>
+
         </section>
       </div>
     </>

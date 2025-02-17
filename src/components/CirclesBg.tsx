70e795db9406252487e5ab4c2d3cfca689ad01ba
CactusNormal7 <jules.besson7@gmail.com>
2025-02-17 15:40:59 +0100

    fixing gabriel's betise

--- src/components/CirclesBg.tsx
+++ src/components/CirclesBg.tsx
@@ -4,13 +4,13 @@ import React from "react";
 import "../styles/CirclesBg.scss";
 
 export function CirclesBg() {
-	return (
-		<>
-			<div className="circle" id="circle2"></div>
-			<div className="circle" id="circle1"></div>
-			<div className="circle" id="circle4"></div>
-			<div className="circle" id="circle3"></div>
-			<img src="guitar.svg" alt="" />
-		</>
-	);
+  return (
+    <>
+      <div className="circle" id="circle2"></div>
+      <div className="circle" id="circle1"></div>
+      <div className="circle" id="circle4"></div>
+      <div className="circle" id="circle3"></div>
+      <img className="guitar-svg" src="guitar.svg" alt="" />
+    </>
+  );
 }

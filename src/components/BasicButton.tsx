45524cd2e221fed1f09bcc8eef7084c7eec40176
CactusNormal7 <jules.besson7@gmail.com>
2025-02-17 15:09:42 +0100

    save

--- src/components/BasicButton.tsx
+++ src/components/BasicButton.tsx
@@ -11,7 +11,7 @@ export function BasicButton({ text, ...props }: BasicButtonProps) {
   return (
     <button
       {...props}
-      className="px-[25px] py-[10px] rounded bg-[#272936] text-[#a3a5b1] font-semibold cursor-pointer transition-transform duration-200 active:scale-90 hover:font-bold"
+      className="px-[25px] py-[10px] rounded bg-[#272936] text-[#a3a5b1] font-semibold cursor-pointer transition-all duration-200 active:scale-90 hover:bg-black"
     >
       {text}
     </button>

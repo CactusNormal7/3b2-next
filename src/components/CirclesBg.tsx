d45e3337a08c27089fdbba411f162e7c9640611f
Gabriel MARIE <gaby01@youmail.fr>
2025-02-17 12:01:47 +0100

    added CirclesBg component (bacground of the Homepage)

---
+++ src/components/CirclesBg.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import "../styles/CirclesBg.scss";
+
+export function CirclesBg() {
+	return (
+		<>
+			<div className="circle" id="circle2"></div>
+			<div className="circle" id="circle1"></div>
+			<div className="circle" id="circle4"></div>
+			<div className="circle" id="circle3"></div>
+		</>
+	);
+}
